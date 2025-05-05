2901dd70cdab2a0783063a090726117fedc91c40
Andrei Lahunovich <lagunmelman@gmail.com>
2025-05-05 19:20:54 +0300

    Начальный main.py для проверки работы приложения

--- app/main.py
+++ app/main.py
@@ -1,8 +1,33 @@
+from contextlib import asynccontextmanager
+
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+
+from core.config import settings
 
 
-def main():
-    print("Hello from managepro!")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+
+
+main_app = FastAPI(
+    default_response_class=ORJSONResponse,
+    lifespan=lifespan,
+    title="ManagePro",
+)
+
+
+@main_app.get("/")
+async def root():
+    return {"message": "Hello"}
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(
+        "main:main_app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
+    )
