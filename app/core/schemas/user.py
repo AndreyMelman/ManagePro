42704f0cf008676d56487b770bac353172a8b68a
Andrei Lahunovich <lagunmelman@gmail.com>
2025-05-06 15:56:46 +0300

    Добавил дополнительные схемы для Роли

--- app/core/schemas/user.py
+++ app/core/schemas/user.py
@@ -1,9 +1,15 @@
+from typing import Annotated
+
+from fastapi import Depends
 from fastapi_users import schemas
+from pydantic import BaseModel
+
+from core.types.role import UserRole
 from core.types.user_id import UserIdType
 
 
 class UserRead(schemas.BaseUser[UserIdType]):
-    pass
+    role: Annotated[UserRole, Depends()]
 
 
 class UserCreate(schemas.BaseUserCreate):
@@ -12,3 +18,11 @@ class UserCreate(schemas.BaseUserCreate):
 
 class UserUpdate(schemas.BaseUserUpdate):
     pass
+
+
+class UpdateRoleRequest(BaseModel):
+    role: Annotated[UserRole, Depends()]
+
+
+class SuperUserCreate(UserCreate):
+    role: Annotated[UserRole, Depends()]
