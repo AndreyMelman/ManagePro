0711b5b6c7f314859fd1171956e216b6419414cb
Andrei Lahunovich <lagunmelman@gmail.com>
2025-05-06 15:58:38 +0300

    + Функция для проверки роли пользователя

--- app/api/api_v1/fastapi_users.py
+++ app/api/api_v1/fastapi_users.py
@@ -1,6 +1,8 @@
+from fastapi import Depends, HTTPException, status
 from fastapi_users import FastAPIUsers
 
 from core.models import User
+from core.types.role import UserRole
 from core.types.user_id import UserIdType
 
 from api.dependencies.authentication import get_user_manager
@@ -13,3 +15,18 @@ fastapi_users = FastAPIUsers[User, UserIdType](
 
 current_active_user = fastapi_users.current_user(active=True)
 current_active_superuser = fastapi_users.current_user(active=True, superuser=True)
+
+
+async def get_user_with_role(
+    role: UserRole, user: User = Depends(current_active_user)
+) -> User:
+    if user.role != role:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=f"User must have {role} role",
+        )
+    return user
+
+
+async def get_manager(user: User = Depends(current_active_user)) -> User:
+    return await get_user_with_role(UserRole.MANAGER, user)
