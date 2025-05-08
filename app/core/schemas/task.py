9c692b7796bf2ed7ad6752f4ba46c4b7bf352c02
Andrei Lahunovich <lagunmelman@gmail.com>
2025-05-09 01:43:36 +0300

    Схемы для задач

--- app/core/schemas/task.py
+++ app/core/schemas/task.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+from typing import Annotated
+
+from pydantic import BaseModel, Field
 from sqlalchemy import Enum
 
 
@@ -5,3 +9,11 @@ class TaskStatus(str, Enum):
     OPEN = "open"
     IN_PROGRESS = "in_progress"
     COMPLETED = "completed"
+
+
+#
+# class TaskBaseSchema(BaseModel):
+#     title: Annotated[str, Field(min_length=1, max_length=250)]
+#     description: Annotated[str | None, Field(max_length=250)] = None
+#     deadline: Annotated[datetime | None, Field()] = "2025-01-01T00:00:00"
+#     status: Annotated[TaskStatus, Field()] = TaskStatus.OPEN
