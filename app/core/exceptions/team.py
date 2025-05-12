3f76ae2487b3d19aae773c4d28f1ef5056a9fa08
Andrei Lahunovich <lagunmelman@gmail.com>
2025-05-12 20:53:02 +0300

    Создал отдельный модуль для ошибок

---
+++ app/core/exceptions/team.py
@@ -0,0 +1,65 @@
+from fastapi import HTTPException, status
+
+
+class TeamNotFoundError(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Команда не найдена",
+        )
+
+
+class UserNotFoundError(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Пользователь не найден",
+        )
+
+
+class TeamAccessDeniedError(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="У вас нет доступа к этой команде",
+        )
+
+
+class TeamAdminRequiredError(HTTPException):
+    def __init__(self, team_name: str):
+        super().__init__(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=f"Только админ команды {team_name} может выполнять это действие",
+        )
+
+
+class UserAlreadyInTeamError(HTTPException):
+    def __init__(self, user_id: int):
+        super().__init__(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"У пользователя {user_id} уже есть команда",
+        )
+
+
+class UserNotInTeamError(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Пользователь не состоит в этой команде",
+        )
+
+
+class CannotRemoveTeamAdminError(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Нельзя удалить админа команды",
+        )
+
+
+class TeamCodeExistsError(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Код команды уже существует",
+        )
