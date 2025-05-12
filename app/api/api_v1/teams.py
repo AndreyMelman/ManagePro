625182ff4ada68eb7c326614f141fd33eb2bfe4a
Andrei Lahunovich <lagunmelman@gmail.com>
2025-05-12 18:00:18 +0300

    Добавил эндпоинты для получения списка пользователей команды и удаления пользователя из команды

--- app/api/api_v1/teams.py
+++ app/api/api_v1/teams.py
@@ -6,10 +6,24 @@ from api.dependencies.params import (
     CurrentActiveUser,
 )
 from core.schemas.team import TeamCreateSchema, TeamSchema
-from core.schemas.user import UserCreate
+from core.schemas.user import UserCreate, UserRead
 from crud import teams
 
 router = APIRouter(tags=["Teams"])
+
+
+@router.get(
+    "/",
+)
+async def get_team_with_users(
+    crud: TeamServiceDep,
+    user: CurrentActiveAdmin,
+    team_id: int,
+):
+    return await crud.get_team_with_users(
+        team_id=team_id,
+        current_user=user,
+    )
 
 
 @router.post(
@@ -39,6 +53,23 @@ async def add_user_to_team(
     user_id: int,
 ):
     return await crud.add_user_to_team(
+        current_user=user,
+        team_id=team_id,
+        user_id=user_id,
+    )
+
+
+@router.delete(
+    "/{team_id}/user/{user_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+async def remove_user_from_team(
+    crud: TeamServiceDep,
+    user: CurrentActiveAdmin,
+    team_id: int,
+    user_id: int,
+):
+    return await crud.remove_user_from_team(
         current_user=user,
         team_id=team_id,
         user_id=user_id,
