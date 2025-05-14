b744ec4eca2ee602ce99ba7cd581e73bdc8867ce
Andrei Lahunovich <lagunmelman@gmail.com>
2025-05-15 00:23:50 +0300

    Создал свои собственные исключения

---
+++ app/exceptions/team_exceptions.py
@@ -0,0 +1,26 @@
+from .base_exceptions import ServiceError
+
+
+class TeamNotFoundError(ServiceError):
+    pass
+
+
+class TeamAccessDeniedError(ServiceError):
+    pass
+
+
+class TeamAdminRequiredError(ServiceError):
+    def __init__(self, team_name: str):
+        self.team_name = team_name
+
+
+class CannotRemoveTeamAdminError(ServiceError):
+    pass
+
+
+class TeamCodeExistsError(ServiceError):
+    pass
+
+
+class CannotAddTeamAdminError(ServiceError):
+    pass
---
+++ app/exceptions/user_exceptions.py
@@ -0,0 +1,18 @@
+from exceptions.base_exceptions import ServiceError
+
+
+class UserNotFoundError(ServiceError):
+    pass
+
+
+class UserAlreadyInTeamError(ServiceError):
+    def __init__(self, user_id: int):
+        self.user_id = user_id
+
+
+class UserNotInTeamError(ServiceError):
+    pass
+
+
+class UserCannotChangeRole(ServiceError):
+    pass
