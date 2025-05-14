77163ffba924bfd52f8a79eb36181bcbaa3b6fa4
Andrei Lahunovich <lagunmelman@gmail.com>
2025-05-15 00:23:00 +0300

    Зависимость для TaskService

--- app/api/dependencies/params.py
+++ app/api/dependencies/params.py
@@ -10,11 +10,13 @@ from api.api_v1.fastapi_users import (
 )
 from api.dependencies.dependencies import make_crud_dependency
 from core.models import User
+from crud.tasks import TaskService
 from crud.teams import TeamService
 from crud.users import UserService
 
 TeamServiceDep = Annotated[TeamService, Depends(make_crud_dependency(TeamService))]
 UserServiceDep = Annotated[UserService, Depends(make_crud_dependency(UserService))]
+TaskServiceDep = Annotated[TaskService, Depends(make_crud_dependency(TaskService))]
 CurrentActiveUser = Annotated[User, Depends(current_active_user)]
 CurrentActiveManager = Annotated[User, Depends(get_manager)]
 CurrentActiveAdmin = Annotated[User, Depends(get_admin)]
