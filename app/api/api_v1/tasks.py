ea5c28b91d96134761f32b1b953ff9837cde6ff9
Andrei Lahunovich <lagunmelman@gmail.com>
2025-05-15 14:16:38 +0300

    + эндпоинты для получения Задачи по ID, удаления и обновления

--- app/api/api_v1/tasks.py
+++ app/api/api_v1/tasks.py
@@ -1,18 +1,31 @@
-from fastapi import APIRouter
+from typing import Annotated
 
+from fastapi import APIRouter, Path, Depends
+
+from api.dependencies.load_by_id import task_by_id
 from api.dependencies.params import (
     TaskServiceDep,
     CurrentActiveManager,
 )
+from core.models import Task
 from core.schemas.task import (
     TaskSchema,
     TaskCreateShema,
+    TaskUpdateShema,
 )
 from api.docs.teams import (
     TASK_TAG,
 )
+from exceptions.task_exceptions import TaskPermissionError
 
 router = APIRouter(tags=[TASK_TAG])
+
+
+@router.get("/{task_id}", response_model=TaskSchema)
+async def get_task(
+    task: Task = Depends(task_by_id),
+):
+    return task
 
 
 @router.post("", response_model=TaskSchema)
@@ -24,4 +37,19 @@ async def create_task(
     return await crud.create_task(
         current_user=current_user,
         task_in=task_in,
+    )
+
+
+@router.patch("/{task_id}", response_model=TaskSchema)
+async def update_task(
+    crud: TaskServiceDep,
+    current_user: CurrentActiveManager,
+    task_update: TaskUpdateShema,
+    task: Task = Depends(task_by_id),
+):
+    return await crud.update_task(
+        current_user=current_user,
+        task=task,
+        task_update=task_update,
+        partial=True,
     )
