9f66675f0f63ec3b12f4148e2b4819a2fd6a8d57
Andrei Lahunovich <lagunmelman@gmail.com>
2025-05-16 22:02:48 +0300

    + функция для получения команды по id

--- app/api/dependencies/load_by_id.py
+++ app/api/dependencies/load_by_id.py
@@ -1,13 +1,17 @@
 from typing import Annotated
-
 from fastapi import Path
-
-from api.dependencies.params import TaskServiceDep, CurrentActiveManager
-from core.models import Task
+from api.dependencies.params import (
+    TaskServiceDep,
+    CurrentActiveManager,
+    TeamServiceDep,
+    CurrentActiveAdmin,
+)
+from core.models import Task, Team
 from exceptions.task_exceptions import TaskNotFoundError
+from exceptions.team_exceptions import TeamNotFoundError
 
 
-async def task_by_id(
+async def get_task_by_id(
     task_id: Annotated[int, Path()],
     crud: TaskServiceDep,
     current_user: CurrentActiveManager,
@@ -19,3 +23,17 @@ async def task_by_id(
     if task is not None:
         return task
     raise TaskNotFoundError()
+
+
+async def get_team_by_id(
+    team_id: Annotated[int, Path()],
+    crud: TeamServiceDep,
+    current_user: CurrentActiveAdmin,
+) -> Team:
+    team = await crud.get_team(
+        current_user=current_user,
+        team_id=team_id,
+    )
+    if team is not None:
+        return team
+    raise TeamNotFoundError()
