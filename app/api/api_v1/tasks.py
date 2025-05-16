ba0868fafbda004d069d9c589f9804a8ef2b1282
Andrei Lahunovich <lagunmelman@gmail.com>
2025-05-16 22:04:15 +0300

    мелкие изменения форматирования

--- app/api/api_v1/tasks.py
+++ app/api/api_v1/tasks.py
@@ -1,8 +1,6 @@
-from typing import Annotated
+from fastapi import APIRouter, Depends
 
-from fastapi import APIRouter, Path, Depends
-
-from api.dependencies.load_by_id import task_by_id
+from api.dependencies.load_by_id import get_task_by_id
 from api.dependencies.params import (
     TaskServiceDep,
     CurrentActiveManager,
@@ -16,19 +14,24 @@ from core.schemas.task import (
 from api.docs.teams import (
     TASK_TAG,
 )
-from exceptions.task_exceptions import TaskPermissionError
 
 router = APIRouter(tags=[TASK_TAG])
 
 
-@router.get("/{task_id}", response_model=TaskSchema)
+@router.get(
+    "/{task_id}",
+    response_model=TaskSchema,
+)
 async def get_task(
-    task: Task = Depends(task_by_id),
+    task: Task = Depends(get_task_by_id),
 ):
     return task
 
 
-@router.post("", response_model=TaskSchema)
+@router.post(
+    "",
+    response_model=TaskSchema,
+)
 async def create_task(
     crud: TaskServiceDep,
     current_user: CurrentActiveManager,
@@ -40,12 +43,15 @@ async def create_task(
     )
 
 
-@router.patch("/{task_id}", response_model=TaskSchema)
+@router.patch(
+    "/{task_id}",
+    response_model=TaskSchema,
+)
 async def update_task(
     crud: TaskServiceDep,
     current_user: CurrentActiveManager,
     task_update: TaskUpdateShema,
-    task: Task = Depends(task_by_id),
+    task: Task = Depends(get_task_by_id),
 ):
     return await crud.update_task(
         current_user=current_user,
