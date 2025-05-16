3d127fd2fd3fad5322ba096505389ce4240065dc
Andrei Lahunovich <lagunmelman@gmail.com>
2025-05-16 22:00:19 +0300

    Добавил докстринг а также вынес ограничения в отдельные функции-модули

--- app/crud/users.py
+++ app/crud/users.py
@@ -1,11 +1,12 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from exceptions.user_exceptions import (
-    UserNotFoundError,
-    UserCannotChangeRole,
-)
 from core.models import User
 from core.schemas.user import UpdateRoleRequest
+
+from .validators.permissions import (
+    ensure_user_exists,
+    disallow_self_role_change,
+)
 
 
 class UserService:
@@ -14,17 +15,26 @@ class UserService:
 
     async def update_user_role(
         self,
-        user_id: int,
         current_user: User,
         role_data: UpdateRoleRequest,
+        user_id: int,
     ) -> None:
+        """
+        Обновить роль пользователя.
+
+        Args:
+            current_user: Текущий администратор команды
+            role_data: Команда где нужно обновить роль пользователя
+            user_id: ID пользователя
+
+        Raises:
+            ensure_user_exists: Если пользователь не состоит в команде
+            disallow_self_role_change: Если пользователь - это администратор
+        """
         user = await self.session.get(User, user_id)
 
-        if not user:
-            raise UserNotFoundError()
-
-        if user.id == current_user.id:
-            raise UserCannotChangeRole()
+        await ensure_user_exists(user)
+        await disallow_self_role_change(user, current_user)
 
         user.role = role_data.role
         await self.session.commit()
