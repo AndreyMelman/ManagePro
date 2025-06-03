603defa71237a2f2ce0c584aae54e13ac16f5b1f
Andrei Lahunovich <lagunmelman@gmail.com>
2025-06-03 21:03:16 +0300

    Добавил функции получения, обновления и удаления комментариев

--- app/crud/task_comments.py
+++ app/crud/task_comments.py
@@ -1,9 +1,13 @@
-from sqlalchemy import select
+from sqlalchemy import select, Result
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import Task, User, TaskComment
-from core.schemas.task_comment import TaskCommentCreateSchema
-from crud.validators.task_validators import check_user_command, check_task_owner
+from core.schemas.task_comment import TaskCommentCreateSchema, TaskCommentUpdateSchema
+from crud.validators.task_validators import (
+    check_user_command,
+    check_task_owner,
+    check_task_comment_owner,
+)
 
 
 class TaskCommentService:
@@ -23,10 +27,22 @@ class TaskCommentService:
             .order_by(TaskComment.created_at.desc())
         )
 
-        result = await self.session.execute(stmt)
+        result: Result = await self.session.execute(stmt)
 
         comments = result.scalars().all()
         return list(comments)
+
+    async def get_task_comment(
+        self,
+        task: Task,
+        current_user: User,
+        comment_id: int,
+    ) -> TaskComment:
+        check_user_command(user=current_user, task=task)
+        stmt = select(TaskComment).where(TaskComment.task_id == task.id, TaskComment.id == comment_id,)
+        result: Result = await self.session.execute(stmt)
+        comment = result.scalars().first()
+        return comment
 
     async def create_task_comment(
         self,
@@ -46,3 +62,30 @@ class TaskCommentService:
         await self.session.refresh(comment)
 
         return comment
+
+    async def update_task_comment(
+        self,
+        comment: TaskComment,
+        current_user: User,
+        comment_update: TaskCommentUpdateSchema,
+    ) -> TaskComment:
+        check_task_comment_owner(current_user, comment)
+
+        update_data = comment_update.model_dump()
+        for name, value in update_data.items():
+            setattr(comment, name, value)
+
+        await self.session.commit()
+        await self.session.refresh(comment)
+
+        return comment
+
+    async def delete_task_comment(
+        self,
+        comment: TaskComment,
+        current_user: User,
+    ) -> None:
+        check_task_comment_owner(current_user, comment)
+
+        await self.session.delete(comment)
+        await self.session.commit()
