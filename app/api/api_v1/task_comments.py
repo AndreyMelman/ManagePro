02f3f808f2945adede757ebd1e10b1f5719cd0e8
Andrei Lahunovich <lagunmelman@gmail.com>
2025-06-03 21:04:39 +0300

    + Роутеры для обновления и удаления комментариев задач

--- app/api/api_v1/task_comments.py
+++ app/api/api_v1/task_comments.py
@@ -1,14 +1,36 @@
 from fastapi import APIRouter, Depends
 
-from api.dependencies.load_by_id import get_task_by_id
+from api.dependencies.load_by_id import get_task_by_id, get_task_comment_by_id
 from api.dependencies.params import CurrentActiveUser, TaskCommentServiceDep
-from core.models import Task
-from core.schemas.task_comment import TaskCommentCreateSchema, TaskCommentSchema
+from core.models import Task, TaskComment
+from core.schemas.task_comment import (
+    TaskCommentCreateSchema,
+    TaskCommentSchema,
+    TaskCommentUpdateSchema,
+)
 
 router = APIRouter(tags=["Task Comments"])
 
 
-@router.post("/{task_id}", response_model=TaskCommentSchema)
+@router.get(
+    "/{task_id}",
+    response_model=list[TaskCommentSchema],
+)
+async def get_task_comments(
+    crud: TaskCommentServiceDep,
+    current_user: CurrentActiveUser,
+    task: Task = Depends(get_task_by_id),
+):
+    return await crud.get_task_comments(
+        task=task,
+        current_user=current_user,
+    )
+
+
+@router.post(
+    "/{task_id}",
+    response_model=TaskCommentSchema,
+)
 async def create_task_comment(
     crud: TaskCommentServiceDep,
     current_user: CurrentActiveUser,
@@ -19,4 +41,33 @@ async def create_task_comment(
         task=task,
         current_user=current_user,
         comment_in=comment_in,
+    )
+
+
+@router.put(
+    "/{task_id}",
+    response_model=TaskCommentSchema,
+)
+async def update_task_comment(
+    crud: TaskCommentServiceDep,
+    current_user: CurrentActiveUser,
+    comment_update: TaskCommentUpdateSchema,
+    comment: TaskComment = Depends(get_task_comment_by_id),
+):
+    return await crud.update_task_comment(
+        comment=comment,
+        current_user=current_user,
+        comment_update=comment_update,
+    )
+
+
+@router.delete("/{task_id}")
+async def delete_task_comment(
+    crud: TaskCommentServiceDep,
+    current_user: CurrentActiveUser,
+    comment: TaskComment = Depends(get_task_comment_by_id),
+):
+    return await crud.delete_task_comment(
+        comment=comment,
+        current_user=current_user,
     )
