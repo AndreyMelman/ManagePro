c06cd5075b41c540439866f27891e817453f7527
Andrei Lahunovich <lagunmelman@gmail.com>
2025-06-04 13:56:14 +0300

    Создал сервис для оценок задач, + добавил метод для создания оценки

---
+++ app/crud/evaluations.py
@@ -0,0 +1,40 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import Evaluation, User, Task
+from core.schemas.evaluation import (
+    EvaluationCreateSchema,
+)
+from crud.validators.evaluation_validators import already_estimated
+
+
+class EvaluationService:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def create_evaluation(
+        self,
+        evaluation_in: EvaluationCreateSchema,
+        current_user: User,
+        task: Task,
+    ) -> Evaluation:
+        stmt = select(Evaluation).where(
+                Evaluation.task_id == task.id,
+                Evaluation.evaluator_id == current_user.id
+            )
+        existing_evaluation = await self.session.execute(stmt)
+        
+        already_estimated(existing_evaluation)
+
+        evaluation = Evaluation(
+            **evaluation_in.model_dump(),
+            evaluator_id=current_user.id,
+            task_id=task.id,
+            user_id=task.assignee_id,
+        )
+
+        self.session.add(evaluation)
+        await self.session.commit()
+        await self.session.refresh(evaluation)
+        return evaluation
+
