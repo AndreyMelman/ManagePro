361f6acee78b5b5da3a12a364d94ca29dab69774
Andrei Lahunovich <lagunmelman@gmail.com>
2025-06-23 14:14:04 +0300

    Добавил роутер для оценок

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -2,6 +2,13 @@ from fastapi import APIRouter, Request
 
 from utils.templates import templates
 
+from .auth.views import router as auth_router
+from .teams.views import router as teams_router
+from .tasks.views import router as tasks_router
+from .task_comments.views import router as task_comments_router
+from .meetings.views import router as meetings_router
+from .evaluations.views import router as evaluations_router
+
 router = APIRouter()
 
 
@@ -15,18 +22,9 @@ def index_page(
     )
 
 
-def include_routers():
-    from .auth.views import router as auth_router
-    from .teams.views import router as teams_router
-    from .tasks.views import router as tasks_router
-    from .task_comments.views import router as task_comments_router
-    from .meetings.views import router as meetings_router
-
-    router.include_router(auth_router)
-    router.include_router(teams_router)
-    router.include_router(tasks_router)
-    router.include_router(task_comments_router)
-    router.include_router(meetings_router)
-
-
-include_routers()
+router.include_router(auth_router)
+router.include_router(teams_router)
+router.include_router(tasks_router)
+router.include_router(task_comments_router)
+router.include_router(meetings_router)
+router.include_router(evaluations_router)
