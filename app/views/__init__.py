c8f62b1c8700a75741dabd6b49854c8052a6bd5c
Andrei Lahunovich <lagunmelman@gmail.com>
2025-06-23 13:15:15 +0300

    + Роутер для views задач и комментариев

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -17,11 +17,14 @@ def index_page(
 
 def include_routers():
     from .auth.views import router as auth_router
-
     from .teams.views import router as teams_router
+    from .tasks.views import router as tasks_router
+    from .task_comments.views import router as task_comments_router
 
     router.include_router(auth_router)
     router.include_router(teams_router)
+    router.include_router(tasks_router)
+    router.include_router(task_comments_router)
 
 
 include_routers()
