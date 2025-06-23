99ff21bd8c685ebfbc7cb1211fcd73202ab2137a
Andrei Lahunovich <lagunmelman@gmail.com>
2025-06-23 12:32:24 +0300

    + Методы для получения email

--- app/crud/users.py
+++ app/crud/users.py
@@ -1,4 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
 from core.models import User
 from core.schemas.user import UpdateRoleRequest
@@ -15,3 +16,13 @@ class UserService:
     ) -> None:
         user.role = role_data.role
         await self.session.commit()
+
+    async def get_user_by_email(self, email: str) -> User | None:
+        stmt = select(User).where(User.email == email)
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
+
+    async def get_user_by_id(self, user_id: int) -> User | None:
+        stmt = select(User).where(User.id == user_id)
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
